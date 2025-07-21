5d74bad36e2267a69cac10da30075585ff407886
Bishal-cs <bishaldas1152006@gmail.com>
2025-07-21 14:19:06 +0530

    add Delete Before

--- Linked List/Doubly_LinkedList.c
+++ Linked List/Doubly_LinkedList.c
@@ -6,3 +6,35 @@ struct node {
     struct node *left;
 };
 struct node *start = NULL;
+
+//Delete Before element---
+void Delete_Before(int value){
+    if(start == NULL || start -> right == NULL || start -> data == value){
+        printf("Delete Not possiable!");
+    }
+    else{
+        struct node *curr = start->right;
+        while(curr -> right != NULL){
+            curr = curr -> right;
+            if(curr -> data == value){
+                struct node *p = curr -> left;
+                if(p -> left == NULL){
+                    p -> right = NULL;
+                    curr -> left = NULL;
+                    free(p);
+                }
+                else{
+                    p -> left -> right = curr;
+                    curr -> left = p -> left;
+                    p -> right = NULL;
+                    p -> left = NULL;
+                    free(p);
+                }
+           }
+        }
+    }
+}
+
+int Delete_After(int value){
+
+}
