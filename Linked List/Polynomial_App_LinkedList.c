d172fbac000b31cfed3ed4fa75cb26cdf218e407
Bishal-cs <bishaldas1152006@gmail.com>
2025-08-07 13:45:47 +0530

    add display:

--- Linked List/Polynomial_App_LinkedList.c
+++ Linked List/Polynomial_App_LinkedList.c
@@ -5,6 +5,7 @@ struct node{
     int Ex;
     struct node *link;
 };
+// Insert Node in polynomial app ---
 struct node *Insert(struct node *start, float Co, int Ex){
     struct node *temp = (struct node *)malloc(sizeof(struct node));
     temp -> Co = Co;
@@ -15,6 +16,7 @@ struct node *Insert(struct node *start, float Co, int Ex){
     }
     else if(Ex == start -> Ex){
         start -> Co += Co;
+        free(temp);
     }
     else{
         struct node *p = start;
@@ -32,11 +34,29 @@ struct node *Insert(struct node *start, float Co, int Ex){
         }
         else if(p -> link -> Ex == Ex){
             p -> link -> Co += Co;
+            free(temp);
         }
         else{
             temp -> link = p -> link;
             p -> link = temp;
         }
     }
+    return start;
+}
+// Display in polynomial app ---
+struct node *Display(struct node *start){
+    struct node *st = start;
+    printf("F(x) = ");
+    while(st != NULL){
+        printf("%.2fx^%d",st-> Co, st-> Ex);
+        if(st -> link != NULL && st -> link -> Co >= (-0)){
+            printf("-");
+        }
+        else{
+            printf("+");
+        }
+        st = st -> link;
+    }
+    printf("\n");
     return start;
 }
