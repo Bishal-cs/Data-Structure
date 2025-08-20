9a9d1920fbe60dcf1e9ec7506547197912cb9fd6
Bishal-cs <bishaldas1152006@gmail.com>
2025-08-20 20:33:38 +0530

    add push

--- Stack/Linked_List_in_Stack.c
+++ Stack/Linked_List_in_Stack.c
@@ -5,3 +5,24 @@ struct stack{
     struct stack *link;
 };
 struct stack *top = NULL;
+
+int is_Empty(){
+    if(top == NULL){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+void Push(int value){
+    struct stack *temp = (struct stack *)malloc(sizeof(struct stack));
+    if(top == NULL){
+        printf("Stack Overflow !");
+        return -1;
+    }
+    else{
+        temp -> link = top;
+        top = temp;
+    }
+}
+
