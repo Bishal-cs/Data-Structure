5792c47e7f843880571bdc1fdd2bcd2dc11948e7
Bishal-cs <bishaldas1152006@gmail.com>
2025-09-21 09:20:07 +0530

    d

--- Queue/Circular_Queue.c
+++ Queue/Circular_Queue.c
@@ -1,2 +1,23 @@
 #include<stdio.h>
 #define MAX 5
+int Queue[MAX];
+int Front = -1, Rear = -1;
+
+int is_Empty(){
+    if(Front == -1)
+        return 1;
+    return 0;
+}
+
+int is_Full(){
+    if(Rear == MAX -1 || Front +1 == Rear)
+        return 1;
+    return 0;
+}
+
+void main(){
+    if(is_Empty())
+        printf("Hi");
+        return;
+    printf("Hello");
+}
